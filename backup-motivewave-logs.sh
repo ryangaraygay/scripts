4951856d709548c253850230bc29a42cb8a7f0f8
Ryan Garaygay <ryangaraygay@users.noreply.github.com>
2025-04-15 11:08:18 -0700

    Update backup-motivewave-logs.sh to preserve file timestamp when copying

--- backup-motivewave-logs.sh
+++ backup-motivewave-logs.sh
@@ -3,7 +3,7 @@
 # Source folder where the files are located
 source_folder="/Users/ryangaraygay/Library/MotiveWave/output"  # **CHANGE THIS TO YOUR SOURCE FOLDER**
 # Destination folder where the files will be copied
-destination_folder="/Users/ryangaraygay/Library/MotiveWave/output/archive"  # **CHANGE THIS TO YOUR DESTINATION FOLDER**
+destination_folder="/Users/ryangaraygay/Library/Mobile Documents/com~apple~CloudDocs/MyFiles/backups/MotiveWave Logs"  # **CHANGE THIS TO YOUR DESTINATION FOLDER**
 
 # Check if the source and destination folders exist
 if [ ! -d "$source_folder" ]; then
@@ -26,7 +26,7 @@ find "$source_folder" -type f -name "output*" | while IFS= read -r file; do
     # Check if the file already exists in the destination folder
     if [ ! -e "$destination_file" ]; then
         # Copy the file
-        cp "$file" "$destination_file"
+        cp -p "$file" "$destination_file"
         echo "Copied: $filename"
     else
         echo "Skipped: $filename (already exists in destination)"
